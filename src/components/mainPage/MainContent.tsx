ff27fdf369eb1eb042cfa1e94e20d3a0c6be7b9b
Kirill <kirill_ozornin@mail.ru>
2024-03-09 12:18:49 +0500

    Refactoring adaptive2

--- src/components/mainPage/MainContent.tsx
+++ src/components/mainPage/MainContent.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState } from "react";
 import "./mainContent.css";
 import "../../utils/TabsLogic";
 import { FirstTabContent } from "../tabsContent/tab-1-content/FirstTabContent";
